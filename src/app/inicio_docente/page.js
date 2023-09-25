2c8a395e41eb56f43ca8753fd967ad52b41d0517
KelvinG09 <108686606+KelvinG09@users.noreply.github.com>
2023-09-25 13:26:37 -0400

    estructura de pagina de inicio docente

--- src/app/inicio_docente/page.js
+++ src/app/inicio_docente/page.js
@@ -1,7 +1,93 @@
-function Inicio_docente() {
+"use client"
+import React from "react";
+import SidebarClose from "@/components/sidebar/sidebarClose/SidebarClose";
+import { IconButton } from "@mui/material";
+import NotificationsIcon from "@mui/icons-material/NotificationsOutlined";
+import 'bootstrap/dist/css/bootstrap.css'
+import Paper from "@mui/material/Paper";
+import Image from "next/image";
+import { useStyles } from "../layout";
+import { Kanit } from "next/font/google";
+
+const kanit = Kanit({ subsets: ['latin'], weight: ["400", "700"] })
+
+const wallpaperStyle = {
+    backgroundColor: "#dcd6f7",
+    minHeight: "100vh",
+    minWidth: "100vw",
+    display: "flex",
+    justifyContent: "left",
+};
+
+const notificationsButtonStyle = {
+    color: "black",
+    marginTop: "5px",
+    width: "35px",
+    height: "35px",
+    display: "flex",
+    marginRight: "24px",
+};
+
+const notificationsIconStyle = {
+    height: "35px",
+    width: "35px"
+};
+
+const labelStyle = {
+    fontWeight: "bold",
+    fontSize: "48px",
+    display: "flex",
+    marginLeft: "40px",
+    marginTop: "10px",
+    justifyContent: "space-between",
+};
+
+const paperStyle = {
+    width: "472px",
+    marginTop: "30px",
+    marginLeft: "60px",
+};
+
+const divUserStyle = {
+    display: "flex",
+    flexDirection: "row"
+};
+
+const userInfoStyle = {
+    fontSize: "26px",
+    marginLeft: "40px",
+    marginTop: "10px"
+};
+
+
+
+
+export default function Inicio_docente() {
+    
+    const classes = useStyles();
+
     return (
-        <div>Inicio_docente</div>
-    )
+        <div style={wallpaperStyle}>
+        <SidebarClose />
+        <Paper elevation={3} className={classes.paperBig}>
+            <div className={kanit.className} style={labelStyle}>
+                Bienvenido!
+                <IconButton style={notificationsButtonStyle}>
+                    <NotificationsIcon style={notificationsIconStyle} />
+                </IconButton>
+            </div>
+            <Paper elevation={0} style={paperStyle}>
+                <div style={divUserStyle}>
+                    <Image src="https://github.com/JuanDanielU/DisBG/blob/main/Empty-profile-picture.png?raw=true" alt="Profile picture" height={150} width={150}
+                    />
+                    <div style={userInfoStyle}>
+                        Nombres y Apellidos<div>Area Academica</div>
+                    </div>
+                </div>
+            </Paper>
+        </Paper>
+    </div>
+);
 }
 
-export default Inicio_docente
+  
