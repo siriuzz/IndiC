3ce907f12a514897fe19b4c4c005cd374ea6075f
JohnFSDev <johnluis22@outlook.com>
2023-09-28 11:25:31 -0400

    admin y docente metodos get

--- api/users/admin.js
+++ api/users/admin.js
@@ -1,3 +1,47 @@
 const app = require('../../express-config');
 const router = app.router;
+const AdminController = require('../controllers/AdminController');
 
+
+router.get('/Administrador', async (req, res) => {
+
+// #swagger.description = 'Endpoint para obtener todos los administradores.'
+    /*	#swagger.responses[200] = {
+            description: 'Administradores obtenidos correctamente.',
+            schema: { $ref: "#/components/schemas/Admin" }
+    } */
+    try {
+        const result = await AdminController.getAllAdmins(req, res);
+        console.log(result);
+        res.json(result);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+
+}).get('/Administrador/:id', async (req, res) => {
+
+    // #swagger.description = 'Endpoint para obtener un Administrador por su ID.'
+    /*	#swagger.responses[200] = {
+            description: 'Administrador obtenido correctamente.',
+            schema: { $ref: "#/components/schemas/Admin" }
+    } */
+    try {
+        const result = await AdminController.getAdminById(req,res);
+        console.log(result);
+        res.json(result);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+
+}).post('/Administrador', (req, res) => {
+
+
+}).put('/Administrador', (req, res) => {
+
+
+}).patch('/Administrador', (req, res) => {
+
+
+});
+
+module.exports = router;
--- api/users/docente.js
+++ api/users/docente.js
@@ -1,11 +1,32 @@
 const app = require('../../express-config');
 const router = app.router;
-
+const DocenteController = require('../controllers/DocenteController');
 
 router.get('/Docentes', async (req, res) => {
-
+    // #swagger.description = 'Endpoint para obtener todos los docentes.'
+    /*	#swagger.responses[200] = {
+            description: 'Docentes obtenidos correctamente.',
+            schema: { $ref: "#/components/schemas/Docente" }
+    } */
     try {
+        const result = await DocenteController.getAllDocentes(req, res);
+        console.log(result);
+        res.json(result);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 
+}).get('/Docentes/:id', async (req, res) => {
+
+    // #swagger.description = 'Endpoint para obtener un Docente por su ID.'
+    /*	#swagger.responses[200] = {
+            description: 'Docente obtenido correctamente.',
+            schema: { $ref: "#/components/schemas/Docente" }
+    } */
+    try {
+        const result = await DocenteController.getDocenteById(req, res);
+        console.log(result);
+        res.json(result);
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
